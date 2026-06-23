19b6d517174cb87b40fa047181207ba991a5e5f6
aksharasloka <aksharasloka@gmail.com>
2026-06-23 16:22:16 -0400

    added datasets and init database script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,15 @@
+-- Drop and recreate database
+DROP DATABASE IF EXISTS "DataWarehouse";
+CREATE DATABASE "DataWarehouse";
 
+-- Connect to the database
+\c "DataWarehouse";
+
+-- Drop and recreate schemas
+DROP SCHEMA IF EXISTS bronze CASCADE;
+DROP SCHEMA IF EXISTS silver CASCADE;
+DROP SCHEMA IF EXISTS gold CASCADE;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
